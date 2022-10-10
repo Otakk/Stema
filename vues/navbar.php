7566ed4c6320faec6710d93d432bfad829835ad1
Raphael <raphael.tisba@epita.fr>
2022-10-10 17:22:00 +0200

    navbar v1.2

--- vues/navbar.php
+++ vues/navbar.php
@@ -12,7 +12,11 @@ $listeCategories = $objetCategorie->listeCategories();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Stema</title>
     <link rel="shortcut icon" href="../images/logo50x50.png" type="image/x-icon">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../style/style.css">
 </head>
 
@@ -29,26 +33,35 @@ $listeCategories = $objetCategorie->listeCategories();
 
                 <hr class="hrLogo">
 
-                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                <!-- align-items-sm-start -->
+                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-sm-start align-items-center pt-2" id="menu">
                     <li class="nav-item redHover">
                         <a href="mesJeux.php" class="nav-link align-middle px-0"><span class="h2Perso ms-1">Mes jeux</span></a>
                     </li>
 
-                    <li>
-                        <a href="listeCategories.php" class="text-decoration-none h2Perso redHover"><span class="h2Perso ms-1">Catégories</span></a>
-                        <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-                            <?php
-                            $listeCategorie = $objetCategorie->listeCategories();
-                            foreach ($listeCategories->{'records'} as $categorie) {
-                            ?>
-                                <li class="w-100">
-                                    <a href="#" class="nav-link subitem px-0"> <span><?= $categorie->{'fields'}->{'Name'}; ?></span></a>
-                                </li>
-                            <?php
-                            }
-                            ?>
-                        </ul>
-                    </li>
+                    <div class="accordion accordion-flush py-4" id="accordionFlushExample">
+                        <div class="accordion-item bg-dark">
+                            <h2 class="accordion-header" id="flush-headingOne">
+                                <button class="accordion-button bg-dark h2Perso text-white px-0 py-2 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                    <a href="listeCategories.php" class="text-decoration-none h2Perso redHover"><span class="h2Perso ms-1">Catégories</span></a>
+                                </button>
+                            </h2>
+                            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body pt-0">
+                                    <?php
+                                    $listeCategorie = $objetCategorie->listeCategories();
+                                    foreach ($listeCategories->{'records'} as $categorie) {
+                                    ?>
+                                        <li class="w-100">
+                                            <a href="#" class="nav-link subitem px-0"> <span><?= $categorie->{'fields'}->{'Name'}; ?></span></a>
+                                        </li>
+                                    <?php
+                                    }
+                                    ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <li>
                         <a href="#" class="nav-link px-0 align-middle redHover">
                             <span class="h2Perso ms-1">Contacts</span></a>

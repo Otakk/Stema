ec9b768888d5e1ae2247b8e69c274c01a34f9df5
Raphael <raphael.tisba@epita.fr>
2022-10-10 12:43:25 +0200

    1er commit

---
+++ vues/index.php
@@ -0,0 +1,21 @@
+<?php
+require_once "navbar.php";
+
+$session = curl_init();
+
+curl_setopt($session, CURLOPT_URL, "https://api.airtable.com/v0/appvQG97bedW0go4Z/Categorie");
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+$auth = "Authorization: Bearer keyUy6gh4qzzh4OLo";
+curl_setopt($session, CURLOPT_HTTPHEADER, ['Content-Type: application/json', $auth]);
+
+$resultat = curl_exec($session);
+curl_close($session);
+
+$array = json_decode($resultat);
+
+var_dump($array);
+
+foreach($array->{'records'} as $value) {
+    echo '<p>' . ($value->{'fields'}->{'Name'} . '</p>');
+}
---
+++ vues/navbar.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-dark navbar-expand-md bg-dark">
+    <a class="navbar-brand titre" href="./index.php">
+        <img src="../../images/logo.png" class="d-inline-block align-top" alt="">
+        <span>Stema</span>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="navbar-collapse collapse" id="navbarNavDropdown">
+        <div class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-item nav-link" href="/vues/utilisateur/profil.php">Profil</a>
+            </li>
+        </div>
+
+        <div class="navbar-nav ml-auto">
+        </div>
+    </div>
+</nav>

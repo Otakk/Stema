87951fa8aa59609240535ca82c9ef70ea94bf4be
Otakk <99323164+Otakk@users.noreply.github.com>
2022-10-11 14:50:13 +0200

    Update modele.php

--- modeles/modele.php
+++ modeles/modele.php
@@ -1,6 +1,6 @@
 <?php 
-require_once "../Modeles/ListeCategories.php";
+require_once "../modeles/ListeCategories.php";
 require_once "../modeles/categorie.php";
-require_once "../Modeles/contact.php";
-require_once "../Modeles/jeux.php";
+require_once "../modeles/contact.php";
+require_once "../modeles/jeux.php";
 ?>
